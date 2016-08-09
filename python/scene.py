0a9c3177ae4a8299f3ec5cca752473d01ccf7b18
Todd Stavish <toddstavish@gmail.com>
2016-08-09 16:34:59 -0400

    current python code

--- python/scene.py
+++ python/scene.py
@@ -64,8 +64,8 @@ if __name__ == "__main__":
     false_pos_counts = []
     false_neg_counts = []
 
-    test_fp = 'Solution_Submission_v4_915.csv'
-    truth_fp = 'Solution_v4_915.csv'
+    test_fp = 'Sub_clean.csv'
+    truth_fp = 'Sol_clean.csv'
     t0 = time.time()
     prop_polys, sol_polys = load_sorted_polygons(test_fp, truth_fp)
     t1 = time.time()
@@ -74,7 +74,6 @@ if __name__ == "__main__":
 
     t0 = time.time()
     test_image_ids = get_image_ids(test_fp)
-    bad_count = 0
     for image_id in test_image_ids:
         print('Imaged ID: ', image_id)
         test_polys = []
@@ -83,7 +82,7 @@ if __name__ == "__main__":
         for poly in image_test_polys:
             test_polys.append(poly['poly'])
         image_truth_polys = [item for item in sol_polys if item["ImageId"] == image_id]
-        if image_truth_polys == []:
+        if not image_truth_polys:
             true_pos_count = 0
             false_pos_count = len(image_test_polys)
             false_neg_count = 0
@@ -94,12 +93,14 @@ if __name__ == "__main__":
         true_pos_counts.append(true_pos_count)
         false_pos_counts.append(false_pos_count)
         false_neg_counts.append(false_neg_count)
-        if (sum(true_pos_counts) > 0 ) and ((sum( false_neg_counts ) > 0 ) and (sum(false_pos_counts) > 0)):
+        print('true pos count: ', true_pos_count)
+        print('false pos count: ', false_pos_count)
+        print('false neg count: ', false_neg_count)
+        if (sum(true_pos_counts) > 0) and ((sum(false_neg_counts) > 0) and (sum(false_pos_counts) > 0)):
             precision = float(sum(true_pos_counts))/float(sum(true_pos_counts)+sum(false_pos_counts))
             recall = float(sum(true_pos_counts))/float(sum(true_pos_counts)+sum(false_neg_counts))
             F1score  = 2.0*precision*recall/(precision+recall)
             print('F1 Score: ', F1score)
-            print('bad count: ', bad_count)
     t1 = time.time()
     total = t1-t0
     print('time of evaluation: ', total)
