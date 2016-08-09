25722200482e035518845c119e917c7de72c5162
dlindenbaum <dlindenbaum@iqt.org>
2016-08-09 16:56:14 -0400

    added scene_DELWithGEOJSON.py

--- python/scene_DELWIthGEOJSON.py
+++ python/scene_DELWIthGEOJSON.py
@@ -5,11 +5,15 @@ import os
 from numpy import array
 from geojson import load
 from shapely.geometry import Polygon
-import numpy as np
+from shapely import wkt
 import pandas as pd
 import time
 import cPickle as pickle
-
+import math
+import numpy as np
+from multiprocessing import Pool, Process
+import csv
+import itertools
 def polygonize(csv_path):
     polys = []
     polys_df = pd.read_csv(csv_path)
@@ -32,7 +36,7 @@ def load_sorted_polygons(test_csv_path, truth_csv_path):
     # Assumes -
     # 1. Polygons are presorted for descending confindence.
     # 2. Chips with no buildings assign zero for the BuildingId
-    return polygonize(test_csv_path), polygonize(truth_csv_path)
+    return importFromGeoJson(test_csv_path), polygonize(truth_csv_path)
 
 def IoU2(test_poly, truth_polys):
     iouList = []
@@ -43,7 +47,7 @@ def IoU2(test_poly, truth_polys):
         #if idx == 74:
         #    pass
         intersectionResult = test_poly.intersection(truth_poly)
-        if intersectionResult.geom_type == 'Polygon':
+        if intersectionResult.geom_type == 'Polygon' or intersectionResult.geom_type == 'MultiPolygon':
             intersectionArea = intersectionResult.area
         else:
             intersectionArea = 0
@@ -77,7 +81,11 @@ def score(test_polys, truth_polys):
         #    pass
         IoUs = IoU2(test_poly, truth_polys)
         #IoUs = map(lambda x:IoU(test_poly,x),truth_polys)
-        maxIoU = max(IoUs)
+        if not IoUs:
+            maxIoU = 0
+        else:
+            maxIoU = max(IoUs)
+
         threshold = 0.5
         if maxIoU >= threshold:
             true_pos_count += 1
@@ -131,6 +139,119 @@ def writeToGeoJson(polysList, geoJsonName):
 
         dataSource.Destroy()
 
+def readWKTCSV(csv_path):
+
+    polys = []
+    polys_df = pd.read_csv(csv_path)
+    image_ids = set(polys_df['ImageId'].tolist())
+    for image_id in image_ids:
+        img_df = polys_df.loc[polys_df['ImageId'] == image_id]
+        building_ids = set(img_df['BuildingId'].tolist())
+        for building_id in building_ids:
+            if building_id != 0:
+                building_df = img_df.loc[img_df['BuildingId'] == building_id]
+                poly = wkt.loads(building_df.iloc[0,2])
+                polys.append({'ImageId': image_id, 'BuildingId': building_id, 'poly': poly})
+    return polys
+
+
+
+
+def writeToCSV_Polygon(polysList, csvFileName):
+    with open(csvFileName, 'wb') as csvFileName:
+        fileWriter = csv.writer(csvFileName, delimiter=';')
+        fileWriter.writerow(['ImageId', 'BuildingId', 'PolygonWKT'])
+        idx = 0
+        for poly in polysList:
+            print(idx)
+            idx = idx + 1
+            #if poly['poly'].is_empty:
+            fileWriter.writerow([poly['ImageId'], poly['BuildingId'], poly['poly'].wkt])
+            #else:
+            #    if poly['poly'].geom_type == 'MultiPolygon':
+            #        for polyPart in poly['poly']:
+            #            xList,yList = polyPart.exterior.coords.xy
+
+            #            for x,y in zip(xList,yList):
+            #                fileWriter.writerow([poly['ImageId'], poly['BuildingId'], '{}'.format(int(x)), '{}'.format(int(y))])
+            #    else:
+            #        xList, yList = poly['poly'].exterior.coords.xy
+            #        for x, y in zip(xList, yList):
+            #            fileWriter.writerow([poly['ImageId'], poly['BuildingId'], '{}'.format(int(x)), '{}'.format(int(y))])
+
+
+
+
+
+def doublePolysList(polysList):
+    newPolysList = []
+    buildingId = 0
+    for poly in polysList:
+        buildingId = buildingId+1
+        poly['buildingId']=buildingId
+        newPolysList.append(poly)
+        buildingId = buildingId+1
+        poly['buildingId']=buildingId
+        newPolysList.append(poly)
+
+    return newPolysList
+
+
+def importFromGeoJson(geoJsonName):
+
+    #driver = ogr.GetDriverByName('geojson')
+    dataSource = ogr.Open(geoJsonName, 0)
+
+    layer = dataSource.GetLayer()
+    print(layer.GetFeatureCount())
+
+    polys =  []
+    image_id = 1
+    building_id = 0
+    for feature in layer:
+        building_id = building_id + 1
+        polys.append({'ImageId': feature.GetField('ImageId'), 'BuildingId': feature.GetField('BuildingId'), 'poly': wkt.loads(feature.GetGeometryRef().ExportToWkt())})
+
+    return polys
+
+import math
+import numpy as np
+
+def evalFunction((image_id, prop_polys, sol_polys)):
+
+    test_polys = []
+    truth_polys = []
+    true_pos_count = 0
+    false_pos_count = 0
+    false_neg_count = 0
+    image_test_polys = [item for item in prop_polys if item["ImageId"] == image_id]
+    for poly in image_test_polys:
+        test_polys.append(poly['poly'])
+
+    image_truth_polys = [item for item in sol_polys if item["ImageId"] == image_id]
+    if image_truth_polys == []:
+        true_pos_count = 0
+        false_pos_count = len(image_test_polys)
+        false_neg_count = 0
+    else:
+        for poly in image_truth_polys:
+            truth_polys.append(poly['poly'])
+
+        true_pos_count, false_pos_count, false_neg_count = score(test_polys, truth_polys)
+
+
+    if (true_pos_count > 0) or ((false_neg_count > 0) and (false_pos_count > 0)):
+
+        precision = float(true_pos_count) / (float(true_pos_count) + float(false_pos_count))
+        recall = float(true_pos_count) / (float(true_pos_count) + float(false_neg_count))
+        F1score = 2.0 * precision * recall / (precision + recall)
+    else:
+        F1score = 0
+    return F1score
+        # print('F1 Score: ', F1score)
+        # print('bad count: ', bad_count)
+
+
 
 
 
@@ -139,14 +260,17 @@ if __name__ == "__main__":
     false_pos_counts = []
     false_neg_counts = []
 
-    test_fp = '/Users/dlindenbaum/Documents/CosmiQCode_09282015/BEE-CSharp/data/Solution_Submission_v4.csv'
-    truth_fp = 'data/Solution_v4.csv'
+
+    test_fp = '../solution2xSubmission.geojson'
+    truth_fp = '../solution_v4Buffer.geojson'
+
 
     pickleLocation = 'solution_v4.p'
     t0 = time.time()
-    sol_polys = pickle.load(open(pickleLocation, "rb"))
-    sol_polys = addbuffer(sol_polys)
-    prop_polys = sol_polys
+    #sol_polys = pickle.load(open(pickleLocation, "rb"))
+    sol_polys = importFromGeoJson(truth_fp)
+
+    prop_polys = importFromGeoJson(test_fp)
 
     #prop_polys, sol_polys = load_sorted_polygons(test_fp, truth_fp)
     t1 = time.time()
@@ -154,33 +278,59 @@ if __name__ == "__main__":
     print('time of ingest: ', total)
 
     t0 = time.time()
-    test_image_ids = get_image_ids(test_fp)
+    test_image_ids = set([item['ImageId'] for item in prop_polys if item['ImageId']>0])
     bad_count = 0
-    for image_id in test_image_ids:
-        print('Imaged ID: ', image_id)
-        test_polys = []
-        truth_polys = []
-        image_test_polys = [item for item in prop_polys if item["ImageId"] == image_id]
-        for poly in image_test_polys:
-            test_polys.append(poly['poly'])
-        image_truth_polys = [item for item in sol_polys if item["ImageId"] == image_id]
-        if image_truth_polys == []:
-            true_pos_count = 0
-            false_pos_count = len(image_test_polys)
-            false_neg_count = 0
-        else:
-            for poly in image_truth_polys:
-                truth_polys.append(poly['poly'])
-            true_pos_count, false_pos_count, false_neg_count = score(test_polys, truth_polys)
-        true_pos_counts.append(true_pos_count)
-        false_pos_counts.append(false_pos_count)
-        false_neg_counts.append(false_neg_count)
-        if (sum(true_pos_counts) > 0 ) and ((sum( false_neg_counts ) > 0 ) and (sum(false_pos_counts) > 0)):
-            precision = float(sum(true_pos_counts))/float(sum(true_pos_counts)+sum(false_pos_counts))
-            recall = float(sum(true_pos_counts))/float(sum(true_pos_counts)+sum(false_neg_counts))
-            F1score  = 2.0*precision*recall/(precision+recall)
-            print('F1 Score: ', F1score)
-            print('bad count: ', bad_count)
+    F1ScoreList=[]
+    p = Pool(processes=8)
+    F1ScoreList = p.map(evalFunction, zip(test_image_ids, itertools.repeat(prop_polys, times=len(test_image_ids)), itertools.repeat(sol_polys, times=len(test_image_ids))))
+    #for image_id in test_image_ids:
+        #p.map(evalFunction, )
+        # print('Imaged ID: ', image_id)
+        # test_polys = []
+        # truth_polys = []
+        # image_test_polys = [item for item in prop_polys if item["ImageId"] == image_id]
+        # for poly in image_test_polys:
+        #     test_polys.append(poly['poly'])
+        # image_truth_polys = [item for item in sol_polys if item["ImageId"] == image_id]
+        # if image_truth_polys == []:
+        #     true_pos_count = 0
+        #     false_pos_count = len(image_test_polys)
+        #     false_neg_count = 0
+        # else:
+        #     for poly in image_truth_polys:
+        #         truth_polys.append(poly['poly'])
+        #     true_pos_count, false_pos_count, false_neg_count = score(test_polys, truth_polys)
+        # true_pos_counts.append(true_pos_count)
+        # false_pos_counts.append(false_pos_count)
+        # false_neg_counts.append(false_neg_count)
+        # if (sum(true_pos_counts) > 0 ) and ((sum( false_neg_counts ) > 0 ) and (sum(false_pos_counts) > 0)):
+        #     precision = float(sum(true_pos_counts))/float(sum(true_pos_counts)+sum(false_pos_counts))
+        #     recall = float(sum(true_pos_counts))/float(sum(true_pos_counts)+sum(false_neg_counts))
+        #     F1score  = 2.0*precision*recall/(precision+recall)
+        #     F1ScoreList.append(F1score)
+        #     #print('F1 Score: ', F1score)
+        #     #print('bad count: ', bad_count)
     t1 = time.time()
     total = t1-t0
     print('time of evaluation: ', total)
+    print(F1ScoreList)
+    print(np.mean(F1ScoreList))
+
+
+    t5 =time.time()
+
+    readWKTCSV('/Users/dlindenbaum/Documents/CosmiQCode_09282015/BEE-CSharp/wktSubmission.csv')
+
+
+    t6 = time.time()
+    print(t6-t5)
+
+
+    t5 = time.time()
+    newTest = []
+    for example in test:
+        example['poly'] = wkt.loads(example['PolygonWKT'])
+
+
+
+    t6 = time.time()
