8de5d466d7e9e25a3fe487621a07cdc3920487e5
dlindenbaum <dlindenbaum@iqt.org>
2016-08-11 19:54:01 -0400

    added convert and copy fixes for full stage

--- python/convert_and_copy_optimized_DEL08102016.py
+++ python/convert_and_copy_optimized_DEL08102016.py
@@ -129,6 +129,8 @@ def pixelToLatLon2(geotifAddr,pixelPairs):
     return latLonPairs
 
 def geoPolygonToPixelPolygonWKT(geom, inputRaster, targetSR, geomTransform):
+    # Returns Pixel Coordinate List and GeoCoordinateList
+
     polygonPixBufferList = []
     polygonPixBufferWKTList = []
     if geom.GetGeometryName()=='POLYGON':
@@ -144,7 +146,8 @@ def geoPolygonToPixelPolygonWKT(geom, inputRaster, targetSR, geomTransform):
 
             polygonPix.AddGeometry(ringPix)
             polygonPixBuffer  = polygonPix.Buffer(0.0)
-            polygonPixBufferList.append(polygonPixBuffer)
+            polygonPixBufferList.append([polygonPixBuffer, geom])
+
     elif geom.GetGeometryName() == 'MULTIPOLYGON':
 
 
@@ -161,15 +164,15 @@ def geoPolygonToPixelPolygonWKT(geom, inputRaster, targetSR, geomTransform):
 
                 polygonPix.AddGeometry(ringPix)
                 polygonPixBuffer = polygonPix.Buffer(0.0)
-                polygonPixBufferList.append(polygonPixBuffer)
+                polygonPixBufferList.append([polygonPixBuffer, geom])
 
 
     for polygonTest in polygonPixBufferList:
-        if polygonTest.GetGeometryName() == 'POLYGON':
-            polygonPixBufferWKTList.append(polygonTest.ExportToWkt())
-        elif polygonTest.GetGeometryName() == 'MULTIPOLYGON':
-            for polygonTest2 in polygonTest:
-                polygonPixBufferWKTList.append(polygonTest2.ExportToWkt())
+        if polygonTest[0].GetGeometryName() == 'POLYGON':
+            polygonPixBufferWKTList.append([polygonTest[0].ExportToWkt(), polygonTest[1].ExportToWkt()])
+        elif polygonTest[0].GetGeometryName() == 'MULTIPOLYGON':
+            for polygonTest2 in polygonTest[0]:
+                polygonPixBufferWKTList.append([polygonTest2.ExportToWkt(), polygonTest[1].ExportToWkt()])
 
 
 
@@ -189,39 +192,146 @@ def geoPolygonToPixelPolygonWKT(geom, inputRaster, targetSR, geomTransform):
 
 if __name__ == "__main__":
 
-    csvFileTotalName_3Band = '../data/all_polygons_solution_3Band.csv'
 
-    csvFileTrainName_3Band = '../data/train_polygons_solution_3Band.csv'
-    csvFilePublicName_3Band = '../data/public_polygons_solution_3Band.csv'
-    csvFilePrivateName_3Band = '../data/private_polygons_solution_3Band.csv'
 
-    csvFileTotalName_8Band = '../data/all_polygons_solution_8Band.csv'
-    csvFileTrainName_8Band = '../data/train_polygons_solution_8Band.csv'
-    csvFilePublicName_8Band = '../data/public_polygons_solution_8Band.csv'
-    csvFilePrivateName_8Band = '../data/private_polygons_solution_8Band.csv'
+    privateList = ['013022232030.tif',
+                '013022232031.tif',
+                '013022232032.tif',
+                '013022232033.tif',
+                '013022232120.tif',
+                '013022232122.tif',
+                '013022232210.tif',
+                '013022232211.tif',
+                '013022232300.tif'
+                   ]
+
+
+    ## write to files
+
+
+
+
+
+    ## writetoDirectory
+    public_8bandRasterLocation = '../data/spacenet/stageToAWS/processedData/8band/'
+    public_3bandRasterLocation = '../data/spacenet/stageToAWS/processedData/3band/'
+    public_geoJsonFileLocation = '../data/spacenet/stageToAWS/processedData/vectorData/geoJson/'
+    public_summaryFiles        = '../data/spacenet/stageToAWS/processedData/vectorData/summaryData/'
+
+    private_8bandRasterLocation = '../data/spacenet/private/processedData/8band/'
+    private_3bandRasterLocation = '../data/spacenet/private/processedData/3band/'
+    private_geoJsonFileLocation = '../data/spacenet/private/processedData/vectorData/geoJson'
+    private_summaryFiles        = '../data/spacenet/private/processedData/vectorData/summaryData/'
+
+    csvFileTotalName_3Band = '../data/spacenet/all_polygons_solution_3Band.csv'
+    csvFilePublicName_3Band = os.path.join(public_summaryFiles, 'public_polygons_solution_3Band.csv')
+    csvFilePrivateName_3Band = os.path.join(private_summaryFiles, 'private_polygons_solution_3Band.csv')
+
+    csvFileTotalName_8Band = '../data/spacenet/all_polygons_solution_8Band.csv'
+    csvFilePublicName_8Band = os.path.join(public_summaryFiles, 'public_polygons_solution_8Band.csv')
+    csvFilePrivateName_8Band = os.path.join(private_summaryFiles, 'private_polygons_solution_8Band.csv')
 
     csvFileImageKeyIndexName = '../data/imageKeyIndex.csv'
+
+    if not os.path.exists(public_8bandRasterLocation):
+        os.makedirs(public_8bandRasterLocation)
+
+    if not os.path.exists(public_3bandRasterLocation):
+        os.makedirs(public_3bandRasterLocation)
+
+    if not os.path.exists(public_geoJsonFileLocation):
+        os.makedirs(public_geoJsonFileLocation)
+
+    if not os.path.exists(public_summaryFiles):
+        os.makedirs(public_summaryFiles)
+
+    if not os.path.exists(private_8bandRasterLocation):
+        os.makedirs(private_8bandRasterLocation)
+
+    if not os.path.exists(private_3bandRasterLocation):
+        os.makedirs(private_3bandRasterLocation)
+
+    if not os.path.exists(private_geoJsonFileLocation):
+        os.makedirs(private_geoJsonFileLocation)
+
+    if not os.path.exists(private_summaryFiles):
+        os.makedirs(private_summaryFiles)
+
+
     aoiFile = '../data/chipNames_inAOI.txt'
     imageFileList = []
     with open(aoiFile, 'rb') as csvfile:
         fileReader = csv.reader(csvfile, delimiter=' ', quotechar='|')
         for row in fileReader:
             imageFileList.append(row[0])
+    privimageId = 0
+    pubimageId = 0
+    imageFileListTranslate = []
+    for imageFile in imageFileList:
+        imageFileSplt = imageFile.split('_')
+        if imageFileSplt[0] in privateList:
+            usage='Private'
+            privimageId = privimageId + 1
+            imageFileBase = "{}_Private_img{}".format(imageFileSplt[1], privimageId)
+        else:
+            usage='Public'
+            pubimageId = pubimageId+1
+            imageFileBase = "{}_Public_img{}".format(imageFileSplt[1], pubimageId)
 
+        imageFileListTranslate[imageFile,
+                               usage,
+                               imageFileBase,
+                               "3band_{}.tif".format(imageFileBase),
+                               "3band_{}_Pix.geojson".format(imageFileBase),
+                               "{}_Geo.geojson".format(imageFileBase),
+                               "8band_{}.tif".format(imageFileBase),
+                               "8band_{}_Pix.geojson".format(imageFileBase),
+                               "{}_Geo.geojson".format(imageFileBase)
+                                ]
+
+    with open(csvFileImageKeyIndexName, 'wb') as csvfileImageKey:
+        writerImageKey = csv.writer(csvfileImageKey, delimiter=',', lineterminator='\n')
+        writerImageKey.writerow(['srcRaster',
+                                 'Usage',
+                                 'imageFileBase'
+                                 '3BandTiffName',
+                                 '3BandGeoJsonPixName',
+                                 '3BandGeoJsonGeoName',
+                                 '8BandTiffName',
+                                 '8BandGeoJsonPixName',
+                                 '8BandGeoJsonGeoName',
+                                 ])
+        for imageFileTranslate in imageFileListTranslate:
+            writerImageKey.writerow(imageFileTranslate)
 
     with open(csvFileTotalName_3Band, 'wb') as csvfileTotal, \
             open(csvFileTotalName_8Band, 'wb') as csvfileTotal_8Band, \
-            open(csvFileImageKeyIndexName, 'wb') as csvfileImageKey:
-
+            open(csvFilePublicName_3Band, 'wb') as csvfilePublic_3Band, \
+            open(csvFilePrivateName_3Band, 'wb') as csvfilePrivate_3Band, \
+            open(csvFilePublicName_8Band, 'wb') as csvfilePublic_8Band, \
+            open(csvFilePrivateName_8Band, 'wb') as csvfilePrivate_8Band:
 
         # instantiate writers
         writerTotal         = csv.writer(csvfileTotal, delimiter=',',lineterminator='\n')
         writerTotal_8Band   = csv.writer(csvfileTotal_8Band, delimiter=',',lineterminator='\n')
-        writerImageKey      = csv.writer(csvfileImageKey, delimiter=',', lineterminator='\n')
 
-        writerTotal.writerow(['ImageId','BuildingId', 'BuildingId', 'PolygonWKT', 'Usage'])
-        writerTotal_8Band.writerow(['ImageId','BuildingId', 'BuildingId', 'PolygonWKT', 'Usage'])
-        writerImageKey.writerow(['ImageID', 'Usage', 'geoJsonSrc', '3BandSrc', '8BandSrc'])
+
+        writerPublic_3band = csv.writer(csvfilePublic_3Band, delimiter=',', lineterminator='\n')
+        writerPrivate_3band = csv.writer(csvfilePrivate_3Band, delimiter=',', lineterminator='\n')
+
+        writerPublic_8band = csv.writer(csvfilePublic_8Band, delimiter=',', lineterminator='\n')
+        writerPrivate_8band = csv.writer(csvfilePrivate_8Band, delimiter=',', lineterminator='\n')
+
+
+        writerTotal.writerow(['ImageId','BuildingId', 'PolygonWKT_Pix', 'PolygonWKT_Geo', 'Usage'])
+        writerTotal_8Band.writerow(['ImageId','BuildingId', 'PolygonWKT_Pix', 'PolygonWKT_Geo', 'Usage'])
+
+
+        writerPublic_3band.writerow(['ImageId', 'BuildingId',  'PolygonWKT_Pix', 'PolygonWKT_Geo'])
+        writerPrivate_3band.writerow(['ImageId','BuildingId',  'PolygonWKT_Pix', 'PolygonWKT_Geo'])
+
+        writerPublic_8band.writerow(['ImageId', 'BuildingId', 'PolygonWKT_Pix', 'PolygonWKT_Geo'])
+        writerPrivate_8band.writerow(['ImageId', 'BuildingId', 'PolygonWKT_Piz', 'PolygonWKT_Geo'])
 
 
         max_buildings = 0
@@ -230,26 +340,15 @@ if __name__ == "__main__":
         #imagefiles = sorted(os.listdir('image_chips_filtered/3band'))
         #geojsonfiles = sorted(os.listdir('image_chips_filtered/geojsons/'))
 
-        np.random.seed(1234)
-        rand_list = np.random.random_sample((len(imageFileList),))
 
         errors = []
-        for image_id in range(1,len(imageFileList)+1):
+        for imageFileTranslate in imageFileListTranslate:
             
-            rand_number = rand_list[image_id-1]
-            if rand_number < 0.18 :
-                usage = 'Public'
-            elif rand_number < 0.6:
-                usage = 'Private'
-            else:
-                usage = 'Train'
-            writerImageKey.writerow([image_id, usage, imageFileList[image_id-1].replace('.tif', '.geojson'),
-                                     imageFileList[image_id-1], imageFileList[image_id-1]])
 
-
-            truthJsonFp = ''.join(['../data/chipsBuildings/',imageFileList[image_id-1].replace('.tif', '.geojson')])
-            inputRaster = ''.join(['../data/image_chips/3band/',imageFileList[image_id-1]])
-            eightbandRaster = ''.join(['../data/image_chips/8band/',imageFileList[image_id-1]])
+            usage = imageFileTranslate[1]
+            truthJsonFp = ''.join(['../data/chipsBuildings/',imageFileTranslate[0].replace('.tif', '.geojson')])
+            inputRaster = ''.join(['../data/image_chips/3band/',imageFileTranslate[0]])
+            eightbandRaster = ''.join(['../data/image_chips/8band/',imageFileTranslate[0]])
 
             print('reading truthJsonFp=%s' % truthJsonFp)
             # load GeoJSON file
@@ -279,22 +378,74 @@ if __name__ == "__main__":
                     polygonWKTList = geoPolygonToPixelPolygonWKT(geom, inputRaster, targetSR, geomTransform)
 
                     for polygonWKT in polygonWKTList:
-                        outputList = [image_id, building_id, polygonWKT, usage]
+                        outputList = [imageFileTranslate[2], building_id, polygonWKT[0], polygonWKT[1], usage]
                         writerTotal.writerow(outputList)
+                        if usage == 'Private':
+                            writerPrivate_3band.writerow(outputList[:-1])
+                        else:
+                            writerPublic_3band.writerow(outputList[:-1])
                     ## Calculate 3 band
                     polygonWKTList_8Band = geoPolygonToPixelPolygonWKT(geom, eightbandRaster, targetSR_8band, geomTransform_8band)
 
                     for polygonWKT in polygonWKTList_8Band:
-                        outputList = [image_id, building_id, polygonWKT, usage]
+                        outputList = [imageFileTranslate[2], building_id, polygonWKT[0], polygonWKT[1], usage]
                         writerTotal_8Band.writerow(outputList)
+                        if usage == 'Private':
+                            writerPrivate_3band.writerow(outputList[:-1])
+                        else:
+                            writerPublic_3band.writerow(outputList[:-1])
+
 
 
 
 
             else:
-                outputList = [image_id, -1, Polygon([(0, 0), (0, 0), (0, 0)]).wkt, usage]
+                outputList = [imageFileTranslate[2], -1, Polygon([(0, 0), (0, 0), (0, 0)]).wkt, Polygon([(0, 0), (0, 0), (0, 0)]).wkt, usage]
                 writerTotal.writerow(outputList)
                 writerTotal_8Band.writerow(outputList)
+                if usage == 'Private':
+                    writerPrivate_3band.writerow(outputList[:-1])
+                else:
+                    writerPublic_3band.writerow(outputList[:-1])
+
+    ### do file transfer
+
+
+
+    for imageFileTranslate in imageFileListTranslate:
+        print('startingCopying')
+
+        usage = imageFileTranslate[1]
+        truthJsonFp = ''.join(['../data/chipsBuildings/', imageFileTranslate[0].replace('.tif', '.geojson')])
+        inputRaster = ''.join(['../data/image_chips/3band/', imageFileTranslate[0]])
+        eightbandRaster = ''.join(['../data/image_chips/8band/', imageFileTranslate[0]])
+
+
+        if usage == 'Public':
+
+            # cp 3bandrasterFile to public/private and rename
+            copyfile(inputRaster, os.path.join(public_3bandRasterLocation, imageFileTranslate[3]))
+
+            # cp 8bandrasterFile to public/private and rename
+            copyfile(eightbandRaster, os.path.join(public_8bandRasterLocation, imageFileTranslate[6]))
+
+            # cp cpGeoJson to 3band/8band public/private
+            copyfile(truthJsonFp, os.path.join(public_geoJsonFileLocation, imageFileTranslate[5]))
+
+        else:
+            # cp 3bandrasterFile to public/private and rename
+            copyfile(inputRaster, os.path.join(private_3bandRasterLocation, imageFileTranslate[3]))
+
+            # cp 8bandrasterFile to public/private and rename
+            copyfile(eightbandRaster, os.path.join(private_8bandRasterLocation, imageFileTranslate[6]))
+
+            # cp cpGeoJson to 3band/8band public/private
+            copyfile(truthJsonFp, os.path.join(private_geoJsonFileLocation, imageFileTranslate[5]))
+
+
+
+
+
 
 
 
